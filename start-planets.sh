5002e08df901041623309d78b0af28476b62ec66
Numenorian <numerion@gmail.com>
2025-04-29 18:43:56 -0500

    mobile dev helper script fix

--- start-planets.sh
+++ start-planets.sh
@@ -8,7 +8,7 @@ tmux has-session -t $SNAME 2>/dev/null
 # $? is the most recent foreground pipeline exit status. [source](https://stackoverflow.com/a/5163260)
 
 if [ $? != 0 ]; then
-    cd ~/game-projects/prototype-planet-orbits
+    # cd ~/game-projects/prototype-planet-orbits
     tmux new-session -d -s $SNAME -n serve
     tmux send-keys -t $SNAME:serve "python3 serve.py" C-m
     tmux new-window -n vim
