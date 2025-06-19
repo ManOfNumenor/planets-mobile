125df182d57b9fd8bc4db8c1fcf53acd869845a2
Christer Kaitila <breakdance@mcfunkypants.com>
2025-06-19 09:25:20 -0700

    galactic nebulae using perlin noise

--- js/starfield.js
+++ js/starfield.js
@@ -18,24 +18,30 @@ function drawStarfield() {
 
 function initStarfields() {
     // renders a vast number of stars once at init
-    console.log("creating starfield background");
     for (let layerNum=0; layerNum < gameOptions.starfieldLayerCount; layerNum++) {
+        console.log("creating starfield background layer "+layerNum);
         cachedStarfields[layerNum] = document.createElement("canvas");
         cachedStarfields[layerNum].context = cachedStarfields[layerNum].getContext('2d');
         // larger than playfield so we can scroll around
         cachedStarfields[layerNum].width = gameOptions.starfieldWidth; //canvas.width * 4;
         cachedStarfields[layerNum].height = gameOptions.starfieldHeight; //canvas.height * 4;
+        if (layerNum==gameOptions.starfieldLayerCount-1) {
+            // fartheset parallax layer also has nebulae
+            drawNebulae(cachedStarfields[layerNum].context);
+        }
+        // fill the layer with stars
         for (var r,g,b,x,y,w,h,n=0; n<gameOptions.starfieldStarsPerLayer; n++) {
-            r = g = b = Math.floor(50 + Math.random()*100); // mostly grey
-            r += Math.floor(Math.random()*50-25); // with a little rainbow
-            g += Math.floor(Math.random()*50-25); // randomness
-            b += Math.floor(Math.random()*50); // boost the blue a bit
-            x = Math.floor(Math.random()*cachedStarfields[layerNum].width);
-            y = Math.floor(Math.random()*cachedStarfields[layerNum].height);
-            w = h = 1; // Math.ceil(Math.random()*2); 
-            if (Math.random()<0.15) w = h = 2; // occasional big stars
-            cachedStarfields[layerNum].context.fillStyle = "rgba("+r+","+g+","+b+",1)";
-            cachedStarfields[layerNum].context.fillRect(x,y,w,h);
-        } // star loop
+                r = g = b = Math.floor(50 + Math.random()*100); // mostly grey
+                r += Math.floor(Math.random()*50-25); // with a little rainbow
+                g += Math.floor(Math.random()*50-25); // randomness
+                b += Math.floor(Math.random()*50); // boost the blue a bit
+                x = Math.floor(Math.random()*cachedStarfields[layerNum].width);
+                y = Math.floor(Math.random()*cachedStarfields[layerNum].height);
+                w = h = 1; // Math.ceil(Math.random()*2); 
+                if (Math.random()<0.15) w = h = 2; // occasional big stars
+                cachedStarfields[layerNum].context.fillStyle = "rgba("+r+","+g+","+b+",1)";
+                cachedStarfields[layerNum].context.fillRect(x,y,w,h);
+            } // star loop
     } // for each layer
 }
+
