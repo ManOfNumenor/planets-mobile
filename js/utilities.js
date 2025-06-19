65c608f3fa894ddece6ec22e4ad74c8f89d9fed4
Christer Kaitila <breakdance@mcfunkypants.com>
2025-06-19 08:51:57 -0700

    hardcoded starfield size (4k)

--- js/utilities.js
+++ js/utilities.js
@@ -2,5 +2,7 @@ var gameOptions = {
     showOrbitDebugInfo: true,
     showPointerDebugInfo: false,
     starfieldStarsPerLayer: 10000,
-    starfieldLayerCount: 3
+    starfieldLayerCount: 3,
+    starfieldWidth: 4000,
+    starfieldHeight: 4000,
 };
