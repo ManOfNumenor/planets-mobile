c36024a7cc919063c5e76a80469f04dae293a5ce
Christer Kaitila <breakdance@mcfunkypants.com>
2025-11-25 16:21:05 -0800

    ships added to titlescreen

--- js/wormhole-effect.js
+++ js/wormhole-effect.js
@@ -24,4 +24,31 @@ function drawWormhole() {
         drawBitmapCenteredWithRotationAndScale(wormholePic, x, y, rot, size);
     }
 
+    // just for fun, add a few ships flying by
+    let rotofs = -Math.PI*2;
+    let rad = Math.min(x,y)/2; // orbit size = 1/4 the smallest screen dimension
+    for (let sx,sy,n=0; n<numParticles; n++) {
+        size = 1;
+        sx = (Math.sin(now/2345) * rad);        
+        sy = (Math.sin(now/1224) * rad);        
+        rot = Math.atan2(sy, sx)+rotofs;
+        size = Math.hypot(sx,sy) / rad;
+        drawBitmapCenteredWithRotationAndScale(fleetPic1, x+sx, y+sy, rot, size);
+        sx = (Math.sin(now/2000+1234.1234) * rad);        
+        sy = (Math.sin(now/2430+2345.2345) * rad);        
+        rot = Math.atan2(sy, sx)+rotofs;
+        size = Math.hypot(sx,sy) / rad;
+        drawBitmapCenteredWithRotationAndScale(fleetPic2, x+sx, y+sy, rot, size);
+        sx = (Math.sin(now/1600+777.777) * rad);        
+        sy = (Math.sin(now/2333+777.777) * rad);        
+        rot = Math.atan2(sy, sx)+rotofs;
+        size = Math.hypot(sx,sy) / rad;
+        drawBitmapCenteredWithRotationAndScale(fleetPic3, x+sx, y+sy, rot, size);
+        sx = (Math.sin(now/765.456) * rad);        
+        sy = (Math.sin(now/1054.234) * rad);        
+        rot = Math.atan2(sy, sx)+rotofs;
+        size = Math.hypot(sx,sy) / rad;
+        drawBitmapCenteredWithRotationAndScale(fleetPic4, x+sx, y+sy, rot, size);
+    }
+
 }
